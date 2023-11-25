f0f4c2c35179ae5dc8f28bc8254c55c2a5778d67
Nebot774 <adriclemenbot@gmail.com>
2023-11-25 12:13:00 +0100

    Commit:Crear el Proyecto en Android Studio:
           Abre Android Studio y crea un nuevo proyecto con el nombre "El Sol".
           Selecciona una plantilla adecuada (por ejemplo, Empty Activity).

---
+++ app/src/main/java/com/example/el_sol/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.el_sol;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); // Asegúrate de tener un layout 'activity_main.xml' en tu carpeta de recursos.
+    }
+
+    // Aquí puedes agregar otros métodos y funcionalidades según tu necesidad.
+}
