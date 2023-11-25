b7b8324606fccae364b83a7651e89ff114046fd3
Nebot774 <adriclemenbot@gmail.com>
2023-11-25 12:18:26 +0100

    Commit: Configurar el Layout Principal:
            Edita el archivo XML del layout principal para incluir un RecyclerView.
            Define el layout como una tabla o una grilla usando GridLayoutManager.

--- app/src/main/java/com/example/el_sol/MainActivity.java
+++ app/src/main/java/com/example/el_sol/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.el_sol;
 import android.os.Bundle;
 import android.app.Activity;
 
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); // Asegúrate de tener un layout 'activity_main.xml' en tu carpeta de recursos.
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        int numberOfColumns = 2; // Ajusta esto según el número de columnas que quieras
+        recyclerView.setLayoutManager(new GridLayoutManager(this, numberOfColumns));
+        // Configura aquí tu adaptador para RecyclerView
+
+
     }
 
     // Aquí puedes agregar otros métodos y funcionalidades según tu necesidad.
