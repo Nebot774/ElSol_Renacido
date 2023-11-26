b66d0477d291c825e44a99e7bccc9642630e4050
Nebot774 <adriclemenbot@gmail.com>
2023-11-26 17:58:07 +0100

    Commit: Implementar Funcionalidades de Copiar y Eliminar:
            En el Toolbar de cada ítem, añade opciones para copiar y eliminar.
            Para la opción de copiar, agrega un nuevo ítem al array de datos y llama a notifyItemInserted().
            Para la opción de eliminar, elimina el ítem del array y llama a notifyItemRemoved().

--- app/src/main/java/com/example/el_sol/SolarImagesAdapter.java
+++ app/src/main/java/com/example/el_sol/SolarImagesAdapter.java
@@ -4,6 +4,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toolbar;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -29,9 +30,43 @@ public class SolarImagesAdapter extends RecyclerView.Adapter<SolarImagesAdapter.
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         ImagenSolar solarImage = solarImages.get(position);
         holder.imageView.setImageResource(ImagenSolar.getImageUrl());
-        // Aquí configuras los elementos del ViewHolder con los datos de solarImage
-        // Configura los listeners para tus eventos
+
+        //configuramos el menu en el Toolbar
+        holder.toolbar.setOnMenuItemClickListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.action_copy) {
+                copyItem(position);
+                return true;
+            } else if (itemId == R.id.action_delete) {
+                deleteItem(position);
+                return true;
+            }
+            return false;
+        });
+
+        holder.toolbar.getMenu().clear();
+        holder.toolbar.inflateMenu(R.menu.menu_item);
+
     }
+
+    //metodo para copiar i eliminar items
+    private void copyItem(int position) {
+        // Suponiendo que getImagenResId() retorna un int que es el ID del recurso de la imagen
+        int imageResId = solarImages.get(position). getImageUrl();
+        String imageName = solarImages.get(position).getName();
+
+        ImagenSolar copiedImage = new ImagenSolar(imageResId, imageName);
+        solarImages.add(position, copiedImage); // Añade el ítem copiado en la misma posición
+        notifyItemInserted(position);
+    }
+
+    //metodo eliminar el item
+    private void deleteItem(int position) {
+        solarImages.remove(position); // Elimina el ítem
+        notifyItemRemoved(position);
+    }
+
+
 
     @Override
     public int getItemCount() {
@@ -41,10 +76,11 @@ public class SolarImagesAdapter extends RecyclerView.Adapter<SolarImagesAdapter.
     //classe viewHolder
     public static class ViewHolder extends RecyclerView.ViewHolder {
         ImageView imageView; // Asegúrate de que imageView sea de tipo ImageView
-
+        Toolbar toolbar;
         public ViewHolder(View itemView) {
             super(itemView);
             imageView = (ImageView) itemView.findViewById(R.id.imageView); // Casting a ImageView
+            toolbar = itemView.findViewById(R.id.toolbar);
             // Inicializar otros componentes, como Toolbar, si es necesario
         }
     }
